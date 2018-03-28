5dad52adc9f77e6e7b656249ad7b4aa2e2d17ce4
pjsheeha@andrew.cmu.edu <pjsheeha@andrew.cmu.edu>
2018-03-28 18:16:14 -0400

    AR project first commit!

---
+++ New Unity Project/Assets/rotator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotator : MonoBehaviour {
+	public int countDown = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		countDown += 1;
+		transform.eulerAngles = new Vector3 (10, countDown, 10);
+	}
+}
