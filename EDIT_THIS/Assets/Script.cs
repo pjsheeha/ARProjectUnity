1c082ad08ad926b3f87ef6b5da569e040f92fc97
pjsheeha@andrew.cmu.edu <pjsheeha@andrew.cmu.edu>
2018-03-28 20:33:12 -0400

    updates

---
+++ EDIT_THIS/Assets/Script.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System;
+using Vuforia;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour {
+	private Anchor anchor;
+	public string state = "1";
+	public List<GameObject> nums = new List<GameObject> ();
+
+	// Use this for initialization
+	void Start () {
+		for (int i =0; i < 5; i++) {
+			GameObject cool = GameObject.FindGameObjectWithTag ("" + i + "");
+			print ("A" + cool.name);
+			nums.Add (cool);
+		}
+		for (int i = 0; i < nums.Count; i++) {
+			print ("B" + nums[i].name); 
+			nums [i].SetActive (false);
+		}
+		stateChange ("1");
+		print (nums);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+
+	}
+	public void stateChange(string buttonName) {
+		state = buttonName;
+		for (int i = 0; i < nums.Count; i++) {
+			if (String.Equals (state, ""+i + "")) {
+				print ("A"+state);
+				nums [i].SetActive (true);
+			}
+			if (!String.Equals (state, ""+i + "")) {
+				print ("B"+state);
+				nums [i].SetActive (false);
+			}
+		}
+	}
+}
